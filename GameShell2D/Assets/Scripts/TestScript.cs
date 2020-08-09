3a9a817bf85a21ef321a8626d3eb1fe2f52fe311
ONYX\jorda <jordan.m.a.johnson@gmail.com>
2020-08-09 16:20:46 -0700

    TEsting a script change

--- GameShell2D/Assets/Scripts/TestScript.cs
+++ GameShell2D/Assets/Scripts/TestScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    // This is a change
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello Werld!");
     }
 
     // Update is called once per frame
