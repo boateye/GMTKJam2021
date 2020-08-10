448906f268371a4dd908ae944062f8eb4e58294f
ONYX\jorda <jordan.m.a.johnson@gmail.com>
2020-08-10 01:16:23 -0700

    Added simple screen transitions and TMPro labels

---
+++ GameShell2D/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeScene(int sceneBuildID)
+    {
+        SceneManager.LoadScene(sceneBuildID);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
