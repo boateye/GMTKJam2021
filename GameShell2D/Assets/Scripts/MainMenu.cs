f61855504eb694193e4ab0ad336de570b12ee084
ONYX\jorda <jordan.m.a.johnson@gmail.com>
2020-08-11 19:15:25 -0700

    Main Menu Fades in but doesn't fade on transition to a new scene. Check the SceneChanger and Main Menu Script for the commented code that isn't working as intended.

--- GameShell2D/Assets/Scripts/MainMenu.cs
+++ GameShell2D/Assets/Scripts/MainMenu.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    Animator transitionImage;
+    float transitionTime = 1f;
     public void StartGame()
     {
-        SceneChanger.NextScene();
+        SceneChanger.NextScene(/*transitionImage, transitionTime*/);
+
+        // Attempting to call the corouting here since it wasn't working in the SceneChanger script due to NextScene() being static
+        //SceneChanger sc = new SceneChanger();
+        //StartCoroutine(sc.SceneTransition(SceneManager.GetActiveScene().buildIndex + 1, transitionImage, transitionTime));
+
     }
 }
--- GameShell2D/Assets/Scripts/SceneChanger.cs
+++ GameShell2D/Assets/Scripts/SceneChanger.cs
@@ -14,9 +14,25 @@ public class SceneChanger
     {
         SceneManager.LoadScene(sceneBuildID);
     }
-
+    // Overload NextScene() to allow for a version w/ a transition.
     public static void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public static void NextScene(Animator transitionImage, float transitionTime)
+    {
+        //StartCoroutine(SceneTransition(SceneManager.GetActiveScene().buildIndex + 1, transitionImage, transitionTime));
+    }
+   public static IEnumerator SceneTransition(int sceneBuildID, Animator transitionImage, float transitionTime)
+    {
+        // Play animation
+        transitionImage.SetTrigger("Start");
+
+        // Wait for animaiton to finish (would be more robust if we could set this equal to teh length of the animation) 
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load Scene
+        SceneManager.LoadScene(sceneBuildID);
     }
 }
